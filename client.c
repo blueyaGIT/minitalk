f3e7c62eda79d73ef323d5c3147d2010b239c155
Davide Albano <dalbano@1-B-31.42heilbronn.de>
2024-11-01 12:03:58 +0100

    first plan- todo: replace stderr + fix errors

--- client.c
+++ client.c
@@ -6,14 +6,46 @@
 /*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 11:30:25 by dalbano           #+#    #+#             */
-/*   Updated: 2024/11/01 11:43:51 by dalbano          ###   ########.fr       */
+/*   Updated: 2024/11/01 12:02:56 by dalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "printf/ft_printf.h"
+#include <signal.h>
+
+void	send_character(char c, pid_t server_pid)
+{
+	int	i;
+
+	i = 8;
+	while (--i >= 0)
+	{
+		if (c & (1 << i)) // Check each bit
+			kill(server_pid, SIGUSR1); // Send SIGUSR1 for bit '1'
+		else
+			kill(server_pid, SIGUSR2); // Send SIGUSR2 for bit '0'
+		usleep(100); // Delay to avoid sending too quickly
+	}
+	// Indicate end of message
+	kill(server_pid, SIGUSR2); // Sending SIGUSR2 as a message terminator
+}
 
 int	main(int argc, char **argv)
 {
-	
+	pid_t	server_pid;
+	char	*message;
+	size_t	temp;
+
+	temp = -1;
+	if (argc != 3)
+	{
+		ft_printf(stderr, "Usage: %s <server_pid> <string>\n", argv[0]);
+		return (1);
+	}
+	server_pid = ft_atoi(argv[1]);
+	*message = argv[2];
+	while (++temp < ft_strlen(message))
+		send_character(message[temp], server_pid);
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,18 +6,55 @@
 /*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 10:53:19 by dalbano           #+#    #+#             */
-/*   Updated: 2024/11/01 11:43:25 by dalbano          ###   ########.fr       */
+/*   Updated: 2024/11/01 11:58:29 by dalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "printf/ft_printf.h"
+#include <signal.h>
 
-int	main(void)
+#define BUFFER_SIZE 1024
+
+char	g_buffer[BUFFER_SIZE];
+int		g_index = 0;
+
+void	handle_signal(int sig)
 {
-	pid_t	pid;
+	if (sig == SIGUSR1)
+	{
+		buffer[g_index++] = '1'; // Arbitrarily assign '1' for SIGUSR1
+	}
+	else if (sig == SIGUSR2)
+	{
+		buffer[g_index++] = '0'; // Arbitrarily assign '0' for SIGUSR2
+	}
+	// When receiving the end of string signal (e.g., a specific signal), print the buffer
+	if (g_index > 0 && (sig == SIGUSR2))
+	{
+		// Using SIGUSR2 as an indication of end of message
+		buffer[g_index] = '\0'; // Null-terminate the string
+		printf("Received: %s\n", buffer);
+		// Reset for next message
+		g_index = 0;
+	}
+}
+
+int main()
+{
+	pid_t				pid;
+	struct sigaction	sa;
 
 	pid = getpid();
-	ft_printf("Process ID: %d\n", pid);
+	printf("Server PID: %d\n", pid);
+	// Set up signal handlers
+	sa.sa_handler = handle_signal;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	// Keep the server running
+	while (1)
+		pause(); // Wait for signals
 	return (0);
 }
