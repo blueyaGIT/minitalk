285b568867b23104fe40678fe57b9dfcd7683783
Davide Albano <dalbano@2-F-7.42heilbronn.de>
2024-11-03 11:58:32 +0100

    temp commit | can be ignored

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 11:30:25 by dalbano           #+#    #+#             */
-/*   Updated: 2024/11/03 11:52:12 by dalbano          ###   ########.fr       */
+/*   Updated: 2024/11/03 11:57:49 by dalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,3 +73,5 @@ int	main(int argc, char **argv)
 		pause();
 	return (0);
 }
+
+// test
